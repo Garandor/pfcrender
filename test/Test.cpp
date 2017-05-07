8b549f0ca20453b7d9d25a3b6bb72bd84670d48d
Adam Reif <reifad61878@th-nuernberg.de>
2017-05-07 14:48:42 -0400

    test rename

--- test/Test.cpp
+++ test/Test.cpp
@@ -3,14 +3,14 @@
 #include "xml_listener.h"
 #include "cute_runner.h"
 
-void thisIsATest() {
-	ASSERTM("start writing tests", false);	
+void myTest() {
+	ASSERTM("start writing tests", false);
 }
 
 void runAllTests(int argc, char const *argv[]){
 	cute::suite s;
 	//TODO add your test here
-	s.push_back(CUTE(thisIsATest));
+	s.push_back(CUTE(myTest));
 	cute::xml_file_opener xmlfile(argc,argv);
 	cute::xml_listener<cute::ide_listener<> >  lis(xmlfile.out);
 	cute::makeRunner(lis,argc,argv)(s, "AllTests");
