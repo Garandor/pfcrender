b002b7e7dc87c99593e108289610c7d8e07a9d06
Adam Reif <reifad61878@th-nuernberg.de>
2017-05-25 23:13:58 -0400

    Modify Travis CI to output gh-pages

--- doc/deploy-doxygen-to-gh-pages.sh
+++ doc/deploy-doxygen-to-gh-pages.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
 set -o errexit
-
-rm -rf public
-mkdir public
+set -o nounset
 
 # config
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 
-# build (CHANGE THIS)
-make
+cd bin
+
+make doc
 
 # deploy
-cd public
+cd doc/doc/html
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://garandor:${GITHUB_TOKEN}@$github.com/garandor/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
