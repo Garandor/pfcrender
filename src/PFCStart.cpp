7c4b88c78c8c859fc46c7b232a7104ff36e178e7
Adam Reif <reifad61878@th-nuernberg.de>
2017-08-25 01:07:36 +0100

    WIP App starter implementation

--- src/PFCStart.cpp
+++ src/PFCStart.cpp
@@ -1,8 +1,3 @@
-#include <string>
-#include <vector>
-#include <list>
-#include <iostream>
-#include <assert.h>
 
 #include "PFCStart.h"
 
@@ -16,7 +11,22 @@ void PFCStart::do_stuff()
 
 int main(int argc, void** argv)
 {
-		
+	Plugin_Registry plugins = Plugin_Registry.getInstance();
+	
+	//open config file
 
+	//populate registry
+
+		
+	if(argc=0){
+		QGuiApplication qapp = new QGuiApplication(argc,argv);
+		QQmlApplicationEngine qeng = new QQmlApplicationEngine();
+		qeng.load(main.qml);
+	}
+	else()
+	{
+
+	}
+	
 	return 0;
 }
