7cbfeb477282a3316c780721e295f75946438c34
Adam Reif <reifad61878@th-nuernberg.de>
2017-08-27 22:31:05 +0100

    Reduced PFCStart to simple main. Added qml/qrc for Hello World Demo, Suppress QtCreator CMakeLists parse result

--- src/PFCStart.cpp
+++ src/PFCStart.cpp
@@ -4,33 +4,22 @@
 #include <QString>
 #include <QQmlApplicationEngine>
 #include "Plugin/Plugin_Registry.h"
-#include "PFCStart.h"
-
-PFCStart::PFCStart()
-{
-}
-
-void PFCStart::do_stuff()
-{
-}
 
 int main(int argc, char** argv)
 {
-	Plugin::Plugin_Registry* plugins = Plugin::Plugin_Registry::getInstance();
+    Plugin::Plugin_Registry* plugins = Plugin::Plugin_Registry::getInstance();
 	
 	//open config file
 
 	//populate registry
 
-	//XXX: Needs better distinction between CLI and GUI mode	
-	if(argc=0){
+    //XXX: For now, let's not mess with CLI mode
+#if 1
 		QGuiApplication qapp(argc,argv);
-		QQmlApplicationEngine qeng(QUrl(QStringLiteral("qrc:/main.qml")));
+        QQmlApplicationEngine qeng(QUrl(QStringLiteral("qrc:///main.qml")));
 		return qapp.exec();
-		}
-	else
-	{
+#else
 		QCoreApplication qapp(argc,argv);
 		return qapp.exec();
-	}
+#endif
 }
