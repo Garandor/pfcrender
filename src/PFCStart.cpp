13c87ace097fd6c17232a17d7597a377df7a56eb
Adam Reif <reifad61878@th-nuernberg.de>
2017-11-11 05:52:52 +0000

    Add Version Information

--- src/PFCStart.cpp
+++ src/PFCStart.cpp
@@ -10,6 +10,8 @@
 #include "Model/LSYSModel.h"
 #include "ViewModel/CustomGeometryModel.h"
 
+#include "projectinfo.h"
+
 int main(int argc, char** argv)
 {
     Plugins::Plugin_Registry* plugins = Plugins::Plugin_Registry::getInstance();
@@ -20,18 +22,24 @@ int main(int argc, char** argv)
 
     //XXX: For now, let's not mess with CLI mode
 #if 1
+        QGuiApplication app(argc,argv);
+#else
+        QCoreApplication app(argc,argv);
+#endif
+        //Set Version Information
+        app.setApplicationVersion(VERSION_STRING);
+
+
+
+        //Register custom QML Types
         qmlRegisterType<ViewModel::CustomGeometryModel>("sci.pfcrender.customModel", 1, 0, "CustomGeometryModel");
 
 
-        QGuiApplication app(argc,argv);
+#if 1
         QQmlApplicationEngine qeng(QUrl(QStringLiteral("qrc:///main.qml")));
-
-
         QtGUI::PFCRender desktop_obj(&qeng);
+#endif
+
 
         return app.exec();
-#else
-		QCoreApplication qapp(argc,argv);
-		return qapp.exec();
-#endif
 }
