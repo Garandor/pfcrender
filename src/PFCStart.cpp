76a7acf3d2c05b3b5673866d6ce08c9ffeb1e501
Adam Reif <reifad61878@th-nuernberg.de>
2018-02-09 20:09:35 +0000

    Windows specific changes reg OpenGLESv2

--- src/PFCStart.cpp
+++ src/PFCStart.cpp
@@ -15,6 +15,13 @@
 
 int main(int argc, char** argv)
 {
+    //Force Qt to use OpenGLESv2 / ANGLE on windows so we can use QNanoPainter
+    //Must be set before QGuiApplication
+    //http://doc.qt.io/qt-5/windows-requirements.html
+#if defined (WIN32)
+    QCoreApplication::setAttribute(Qt::AA_UseOpenGLES)
+#endif
+
     QGuiApplication app(argc, argv);
     auto cr = Common::Config_Registry::getInstance(); //Note: cr uses plugin_registry so it is implicitly created
     app.setApplicationVersion(VERSION_STRING); //Set Version Information
